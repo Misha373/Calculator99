5b0674de154a1fb77be1acc8f0b6de8bcf0b1785
Instamaikle <misasapka1@gmail.com>
2024-08-19 15:16:07 +0300

    Готова програма Калькулятор для 2 чисел

--- Calculator99/Program.cs
+++ Calculator99/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Програма калькулятор");
+
+Console.WriteLine("Введіть число a:");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введіть число b:");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введіть операццію між числами a і b:");
+
+char operation = Convert.ToChar(Console.ReadLine());
+
+
+
+
+ if (operation == '/')
+{
+  if (b == 0)
+    {
+        Console.WriteLine("Вираз не має змісту");
+    }
+  else
+    {
+        Console.WriteLine($"Рішенням математичної задачі є: {a/b}");
+    }
+}
+else if (operation == '*')
+{
+    Console.WriteLine($"Рішенням математичної задачі є: {a * b}");
+}
+ else if (operation == '-')
+{
+    Console.WriteLine($"Рішенням математичної задачі є: {a - b}");
+}
+else if (operation == '+')
+{
+    Console.WriteLine($"Рішенням математичної задачі є: {a + b}");
+}
+else
+{
+    Console.WriteLine("Вираз не має змісту");
+}
+
